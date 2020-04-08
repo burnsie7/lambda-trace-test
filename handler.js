f6e8f3ba8f179e2a87db50ffe12af37cc16e98e2
bburns-ddog <benjamin.burns@datadoghq.com>
2020-04-07 21:13:13 -0700

    ok

--- handler.js
+++ handler.js
@@ -4,14 +4,13 @@ const { datadog } = require("datadog-lambda-js");
 const tracer = require("dd-trace").init();
 
 // This function will be wrapped in a span
-const longCalculation = tracer.wrap('calculation-long-number', () => {
+const someFunction = tracer.wrap('some-function', () => {
     // An expensive calculation goes here
 });
 
 // This function will also be wrapped in a span
-module.exports.helloWorld = datadog((event, context, callback) => {
-    longCalculation();
-
+module.exports.helloworld = datadog((event, context, callback) => {
+    someFunction();
     callback(null, {
         statusCode: 200,
         body: 'Hello from serverless!'
